847ee9b2b516610f27f8785ef1f6b0ef0349eef1
Evgeniy Bokach <deathstar@MacBook-Air-Evgeniy.local>
2018-05-31 13:21:48 +0300

    T3-106 Remove AssignContactToFolder

--- mail/api/views.py
+++ mail/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import detail_route, list_route
 from rest_framework.exceptions import ValidationError, ParseError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-from mail.api.models import Folder, Contact, AssignContactToFolder, \
+from mail.api.models import Folder, Contact, \
     ModelApiError, Mail, CustomMailbox
 from mail.api.serializers import MailboxSerializer, MailSerializer, \
     FolderSerializer, ContactSerializer, MoveMailsToFolderSerializer, \
@@ -64,7 +64,7 @@ class MailboxViewSet(viewsets.ModelViewSet):
             folder = contact.folder
             folder.messages.add(mail)
             folder.save()
-        except AssignContactToFolder.DoesNotExist:
+        except Exception:
             # TODO Log here
             pass
         finally:
