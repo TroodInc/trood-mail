363ad5bfe5fb2dd60b02fa9197d0da233be9d86a
Alexander Gor <ornot.work@gmail.com>
2018-06-14 16:11:40 +0300

    fabric and fixes for automated builds

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from fabric.api import task
+from fabric.context_managers import lcd
+from fabric.operations import local
+
+@task
+def build():
+    with lcd('./deployment/'):
+        local('docker-compose build')
+        local('docker-compose up -d')
+
+@task
+def test():
+    with lcd('./deployment/'):
+        local('docker-compose exec mail pytest')
+
+
+@task
+def cleanup():
+    with lcd('./deployment/'):
+        local('docker-compose down')
