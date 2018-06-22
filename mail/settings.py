3d1c2adcfe950ed078e4cd93f9cf0418dc0b8534
Alexander Gor <ornot.work@gmail.com>
2018-06-22 12:10:27 +0300

    T3-146. Mail authorization added, owner fields added

--- mail/settings.py
+++ mail/settings.py
@@ -93,25 +93,6 @@ class BaseConfiguration(Configuration):
         }
     }
 
-
-    # Password validation
-    # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
-    AUTH_PASSWORD_VALIDATORS = [
-        {
-            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-        },
-        {
-            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-        },
-    ]
-
     LOGGING = {
         'version': 1,
         'disable_existing_loggers': True,
@@ -169,7 +150,12 @@ class BaseConfiguration(Configuration):
 
     USE_TZ = True
 
+    TROOD_AUTH_SERVICE_URL = os.environ.get('TROOD_AUTH_SERVICE_URL', 'http://authorization.trood:8000/')
+
     REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'trood_auth_client.authentication.TroodTokenAuthentication',
+        ),
         'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
     }
 
