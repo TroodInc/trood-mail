c74186f1e077f85dd6c34e3b4f44de94011e4ab8
Alexander Gor <ornot.work@gmail.com>
2018-07-06 20:19:42 +0300

    T3-268. Authorization settings update

--- mail/settings.py
+++ mail/settings.py
@@ -164,6 +164,9 @@ class BaseConfiguration(Configuration):
         'release': 'dev'
     }
 
+    SERVICE_DOMAIN = os.environ.get("SERVICE_DOMAIN", "MAIL")
+    SERVICE_AUTH_SECRET = os.environ.get("SERVICE_AUTH_SECRET")
+
 
 class Development(BaseConfiguration):
     DEBUG = True
