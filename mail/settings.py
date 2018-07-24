7e0da9816267c7040dfb31c80844fc5d888e9d66
Alexander Gor <ornot.work@gmail.com>
2018-07-24 14:38:20 +0300

    pagination fix

--- mail/settings.py
+++ mail/settings.py
@@ -157,7 +157,6 @@ class BaseConfiguration(Configuration):
             'trood_auth_client.authentication.TroodTokenAuthentication',
         ),
         'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
         'PAGE_SIZE': 10
     }
 
