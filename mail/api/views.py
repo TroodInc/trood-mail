a4daa4e8c79d6cd97f2e443659a476569300e288
Alexander Gor <ornot.work@gmail.com>
2018-07-24 14:38:20 +0300

    pagination fix

--- mail/api/views.py
+++ mail/api/views.py
@@ -5,6 +5,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.exceptions import ValidationError, ParseError
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
@@ -102,6 +103,7 @@ class MailViewSet(viewsets.ModelViewSet):
 
 
 class ChainViewSet(ReadOnlyModelViewSet):
+    pagination_class = LimitOffsetPagination
     permission_classes = (IsAuthenticated,)
 
     def retrieve(self, request, pk):
