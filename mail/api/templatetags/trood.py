8b8f06c1b0bb69f19bc809aff976607ca3d7840c
Alexander Gor <ornot.work@gmail.com>
2019-01-16 23:23:25 +0200

    T#-640. template tag for getting global configs

--- mail/api/templatetags/trood.py
+++ mail/api/templatetags/trood.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -6,3 +7,9 @@ register = template.Library()
 @register.filter(name='get')
 def get(d, k):
     return d.get(k, None)
+
+
+@register.simple_tag(name='global')
+def get_global(name):
+    # @todo: replace with configurable app from TroodLib
+    return settings.GLOBAL_CONFIGURABLE.get(name, None)
